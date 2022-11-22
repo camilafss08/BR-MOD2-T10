da3fa11e330602e1a7765171ceed72b2b42e17a6
camilafss08 <118767309+camilafss08@users.noreply.github.com>
2022-11-22 20:37:35 -0300

    AULA 2: small cactus

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,14 +1,15 @@
 import pygame 
-from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
+from pygame.sprite import Sprite
 
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
+POS_DUCK = Y_POS + 30
 JUMP_VEL = 8.5
 
 
-
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0] 
         self.dino_rect = self.image.get_rect()
@@ -20,13 +21,11 @@ class Dinosaur:
         self.dino_jump = False
         self.dino_duck = False
 
-
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
         self.dino_rect.x = X_POS 
         self.dino_rect.y = Y_POS 
         self.step_index += 1
-
    
     def jump(self):
         self.image = JUMPING
@@ -42,9 +41,8 @@ class Dinosaur:
     def duck(self):
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect.x = X_POS 
-        self.dino_rect.y = Y_POS 
+        self.dino_rect.y = POS_DUCK
         self.step_index += 1
-        
 
     def update(self, user_input):
         if self.dino_run:
@@ -57,17 +55,20 @@ class Dinosaur:
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
 
-        if user_input[pygame.K_DOWN]:
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
+            self.dino_jump = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
-
+            self.dino_jump = False
     
         if self.step_index >= 10:
             self.step_index = 0
